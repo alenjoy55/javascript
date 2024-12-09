81a2422e24257d26f82f5078bc9813aa7860797f
alenjoy55 <alen85292@gmail.com>
2024-12-09 13:27:40 +0530

    new pro

---
+++ script.js
@@ -0,0 +1,81 @@
+// alert("welcome to all")
+
+// a="hello"
+// console.log("thankyou",a)
+
+// var a="hello"
+// var a=20
+// console.log(a);
+
+// let a=20
+// a="hello"
+// console.log(a);
+
+// const a="welcome"
+// console.log(a);
+
+//number datatype
+// let a
+// a=10
+// console.log(typeof a);
+
+//object datatype
+// let a 
+// a={name:'alen',age:20}
+// console.log(typeof a);
+
+//boolean type
+// let a=true
+// console.log(typeof a);
+
+//array datatype
+// let a=[1,2,3,4,]
+// console.log(typeof a);
+
+// let a 
+// a=10
+// console.log(typeof a);
+// console.log(a);
+
+//arithemetic operator(+,-,*,/,%,**,++,--)
+
+// console.log(10+5);
+
+// let a=5
+// a++
+// a--
+// ++a
+// --a
+// console.log(a);
+
+//assignment operator(+=,-=,*=,/=,%=,**=)
+// let a=10
+// a*=10 
+// console.log(a);
+// let b=5
+// b+=5
+// console.log(b);
+// let c
+// c**=30
+// console.log(c);
+
+//comparision (==,!=,<,>,<=,>=,===,!==)
+let a=10
+let b='20'
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+//logical operators(&&,|| AND,OR)
+// console.log(a!=b && a==10);
+// console.log(a!=b || a==10);
+
+
+
+
+ 
+
+
+
+
+
